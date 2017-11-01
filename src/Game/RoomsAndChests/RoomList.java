cd111b6b5a94c6ca83d7b765cdf0e22ea7bcbd51
Krist <Krist@LAPTOP-BLSH1QC8>
2017-11-01 14:03:39 +0100

    Merge origin/master
    Conflicts:
    	src/Game/Game.java

--- src/Game/RoomsAndChests/RoomList.java
+++ src/Game/RoomsAndChests/RoomList.java
@@ -208,6 +208,9 @@ public class RoomList {
         
         // secret room number 22 
         rooms.get(22).setNorth(rooms.get(12));
+        rooms.get(22).setSouth(rooms.get(12));
+        rooms.get(22).setEast(rooms.get(12));
+        rooms.get(22).setWest(rooms.get(12));
         /**
          * Adds items to each rooms inventory
          */
