cc0a44f2b8c04231bc9f08738e6e3f46edcd3bac
Jollys <Jollys@10.50.130.216>
2017-11-01 13:05:47 +0100

    Merge origin/master
    Conflicts:
    	src/Game/RoomsAndChests/RoomList.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -68,9 +68,7 @@ public class Game {
             io.put(player.currentRoom.getroomInventory().printInventory());
 
             int select = io.select("which way do you wanna go?", l, "");
-            
-//            io.put("index of =" + player.inventory.inventory.indexOf(itemList.getKey()));
-//            System.out.println("");
+          
           
             if ( 
             player.currentRoom.getRoomName().equals("Secret room")
