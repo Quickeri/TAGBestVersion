f3bce520e3fe52738c84a2877bf9abe1626e46ef
Jesper H <Jesper H@10.50.130.176>
2017-11-03 12:10:34 +0100

    JUnit test of combat

---
+++ test/Game/CombatTest.java
@@ -0,0 +1,29 @@
+package Game;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CombatTest {
+
+    @Test
+    public void testFight() {
+        Player p1 = new Player("testuser1", null, 100, 100, null, 20);
+        Monster m1 = new Monster("testmonster", null, 100, 10, null);
+        Combat combat = new Combat(p1, m1);
+        combat.fight();
+        assertEquals(p1.getName(), combat.getWinner());
+
+    }
+
+    @Test
+    public void testFight2() {
+        Player p1 = new Player("testuser1", null, 100, 100, null, 20);
+        Monster m1 = new Monster("testmonster", null, 100, 50, null);
+        Combat combat = new Combat(p1, m1);
+        combat.fight();
+        assertEquals(m1.getName(), combat.getWinner());
+
+    }
+
+}
