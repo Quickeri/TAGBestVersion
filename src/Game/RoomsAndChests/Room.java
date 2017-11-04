4fd45af64f03c84889355b896a36b7991b022493
Krist <Krist@10.50.137.4>
2017-11-04 17:50:19 +0100

    Merge origin/master
    Conflicts:
    	src/Game/Game.java
    	src/Game/Monster.java
    	src/Game/RoomsAndChests/Room.java
    	src/Game/RoomsAndChests/Souts.java

--- src/Game/RoomsAndChests/Room.java
+++ src/Game/RoomsAndChests/Room.java
@@ -11,7 +11,6 @@ public class Room {
     private String description, roomName;
     private Room north, south, east, west;
     Scanner sc = new Scanner(System.in);
-    private ArrayList<Chest> Chestlist = new ArrayList<>();
     private ArrayList<Item> inventory = new ArrayList<>();
     private boolean hashMonster = false;
 
@@ -146,57 +145,7 @@ public class Room {
 
     enum action_type {
         No_operation, Win, Lost, Draw, item
-    };
-
-    public ArrayList<Chest> getChest() {
-        return Chestlist;
     }
 
-    public void addChest(Chest chest) {
-        this.Chestlist.add(chest);
-    }
-
-    public ArrayList<Chest> getChestlist() {
-        return Chestlist;
-    }
-
-    public void setChestlist(ArrayList<Chest> Chestlist) {
-        this.Chestlist = Chestlist;
-    }
-
-    public action_type Chestchoice() {
-        if (Chestlist.size() > 0) {
-            System.out.println(Chestlist + " = list of chests in this room");
-        }
-        if (Chestlist.size() > 0) {
-            System.out.println("do you want to open a chest?");
-            System.out.println("y/n?");
-            String y_n = sc.next();
-            if (y_n.equals("y")) {
-                int Chest_number = 0;
-                boolean choice_ok;
-                if (Chest_number > 1) {
-                    do {
-                        System.out.println("which chest? " + Chest_number + " " + Chestlist);
-                        Chest_number = sc.nextInt();
-
-                        choice_ok = (0 <= Chest_number)
-                                && (Chest_number < Chestlist.size());
-                    } while (!choice_ok);
-                }
-                if (Chestlist.get(Chest_number).item.equals("gold")) {
-                    return action_type.Win;
-
-                } else if (Chestlist.get(Chest_number).item.equals("poison")) {
-                    return action_type.Lost;
-                } else if (Chestlist.get(Chest_number).item.equals("item")) {
-                    return action_type.item;
-                }
-
-            } // if chest chosen
-        }
-        // non-mpty chestlist
-
-        return action_type.No_operation;// Chestchoise
-    } // public class Room
+    
 }
